d829cf9177da37dca0fdb9521fda45d037ecc45c
wasabina67 <wasabina67@gmail.com>
2024-12-30 22:57:35 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-})
+  build: {
+    outDir: 'docs',
+  },
+  base: mode === 'production' ? '/vite-vue-example/' : '/',
+}))
